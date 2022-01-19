df400acc9b081fadafe557c2afb6079479dad036
jeangyu <gjwlsrb1700@naver.com>
2022-01-20 00:54:31 +0900

    Add: 프로그래머스 K번째 수

---
+++ 정렬/프로그래머스_K번째수.py
@@ -0,0 +1,4 @@
+def solution(array, commands): 
+    return [(sorted(array[command[0]-1:command[1]])[command[2]-1]) for command in commands]
+
+print(solution([1, 5, 2, 6, 3, 7, 4], [[2, 5, 3], [4, 4, 1], [1, 7, 3]]))
