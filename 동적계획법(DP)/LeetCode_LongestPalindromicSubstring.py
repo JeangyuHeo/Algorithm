ccb475bd67e124777df1eb96e2a8e1b760114724
jeangyu <gjwlsrb1700@naver.com>
2022-02-14 23:54:40 +0900

    Add: LeetCode Longest Palindromic Substring

---
+++ 동적계획법(DP)/LeetCode_LongestPalindromicSubstring.py
@@ -0,0 +1,14 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        if len(s) <= 1:
+            return s
+        i,l=0,0
+        for j in range(len(s)):
+            if s[j-l: j+1] == s[j-l: j+1][::-1]:
+                i, l = j-l, l+1
+                
+            elif j-l > 0 and s[j-l-1: j+1] == s[j-l-1: j+1][::-1]:
+                i, l = j-l-1, l+2
+                
+        return s[i: i+l]
+        
