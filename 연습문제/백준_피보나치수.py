7b7db44dd9a6e8a4301b2f7a9c41550844edef8a
Jeangyu <gjwlsrb1700@naver.com>
2022-03-07 20:02:02 +0900

    Add: 백준 피보나츠 수

---
+++ 연습문제/백준_피보나치수.py
@@ -0,0 +1,11 @@
+def fibonacci(n):
+    if d[n] != 0:
+        return d[n]
+    else:
+        d[n] = fibonacci(n-2)+ fibonacci(n-1)
+        return d[n]
+    
+num = int(input())
+d = [1 if i ==1 or i==2 else 0 for i in range(num+1) ]
+print(d)
+print(fibonacci(num))
