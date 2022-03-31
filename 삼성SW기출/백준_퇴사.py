0257ae0b02a1cc16a343a3051a1bd470e02ebcf9
Jeangyu <gjwlsrb1700@naver.com>
2022-03-31 15:08:55 +0900

    Add: 백준 퇴사

--- 삼성SW기출/백준_퇴사.py
+++ 삼성SW기출/백준_퇴사.py
@@ -2,11 +2,23 @@ import sys
 
 input = sys.stdin.readline
 
+def solution():
+    
+    for idx in range(n-1, -1, -1):
+        if idx + schedule[idx][0] > n:
+            dp[idx] = dp[idx+1]
+        else:
+            dp[idx] = max(schedule[idx][1] + dp[idx + schedule[idx][0]], dp[idx+1])
+
+    print(max(dp))
+    
 if __name__ == "__main__":
     n = int(input())
     schedule = []
+    answer = 0
+    dp = [0 for _ in range(n+1)]
 
     for _ in range(n):
         schedule.append(list(map(int, input().split(' '))))
 
-    print(n, schedule)
+    solution()
