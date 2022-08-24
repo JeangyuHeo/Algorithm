26653a621a325c915aea671d27284cdef7a2b0fa
Jeangyu <gjwlsrb1700@naver.com>
2022-08-24 15:02:14 +0900

    Add: 백준 타일코드

---
+++ 동적계획법(DP)/백준_타일코드.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+if __name__ == "__main__":
+    n = int(input())
+    dp = [0 for _ in range(n+1)]
+    dp[1], dp[2] = 1, 3
+    
+    for i in range(3, n+1):
+        dp[i] = dp[i-1] + 2*dp[i-2]
+        
+    if n>=3:
+        if n%2:
+            print((dp[n] - dp[(n-1)//2]) // 2 + dp[(n-1)//2])
+        else:
+            print((dp[n]-(2*dp[(n-2)//2] + dp[n//2]))//2 + 2*dp[(n-2)//2] + dp[n//2])
+    else:
+        print(dp[n])
