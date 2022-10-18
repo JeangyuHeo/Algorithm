002a32edd02a37439af209a24bdd5387c7f099a8
Jeangyu <gjwlsrb1700@naver.com>
2022-10-19 01:39:11 +0900

    Add: 백준 행복 유치원

---
+++ 탐욕법/백준_행복유치원.py
@@ -0,0 +1,10 @@
+import sys
+
+input = sys.stdin.readline
+
+if __name__ == "__main__":
+    n, k = map(int, input().split())
+    height = sorted(list(map(int, input().split())))
+    print(sum(sorted([height[i]-height[i-1] for i in range(1, n)])[:n-k]))
+    
+    
