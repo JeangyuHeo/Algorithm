61ae22c540da620060042dbf9d2c618d1b64587a
Jeangyu <gjwlsrb1700@naver.com>
2022-10-19 01:40:19 +0900

    fix: 백준 행복 유치원

--- 탐욕법/백준_행복유치원.py
+++ 탐욕법/백준_행복유치원.py
@@ -6,5 +6,3 @@ if __name__ == "__main__":
     n, k = map(int, input().split())
     height = sorted(list(map(int, input().split())))
     print(sum(sorted([height[i]-height[i-1] for i in range(1, n)])[:n-k]))
-    
-    
